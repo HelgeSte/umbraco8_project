06e845fc8e3ae703d89dbb5234f74cad69b537dc
Helge Stegemoen <helge@stegemoen.com>
2026-05-09 23:16:22 +0200

    Claude ai has finished creating the umbraco 8 project

---
+++ UmbracoV8/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UmbracoV8")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UmbracoV8")]
+[assembly: AssemblyCopyright("Copyright © 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("e013d281-5725-4d6c-9b90-121366746ab1")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
